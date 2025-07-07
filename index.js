f73d25361cc4a8bf3e9c177a6d14724ed2b9cff1
gaboNY123 <gabrinoguera12345@gmail.com>
2025-07-07 10:01:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const bcrypt = require('bcryptjs');
 
 const fs = require('fs');
